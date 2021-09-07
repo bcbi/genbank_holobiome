9c40ed01b285b098e7e6ac102c707353cd712414
vivekramanan <vivek.ramanan14@gmail.com>
2021-09-07 10:46:26 -0400

    changed species Cleaning

--- speciesClean_*_speciesNames.py
+++
@@ -1,4 +0,0 @@
-"""
-Species Clean 1: Creating the text file of species names
-and splitting accordingly 
-"""
---
+++ speciesClean_1_splitSpecies.py
@@ -0,0 +1,25 @@
+"""
+Species Cleaning 1
+Create microSpeciesNames.txt file and splits it accordingly as well
+Author: Vivek Ramanan
+"""
+
+import pandas as pd
+import sys, os
+
+def splitSpecies(dfFile, outputName): 
+    df = pd.read_csv(dfFile, low_memory=False, usecols=['species','host_cleaned'])
+    df = df.dropna(subset=['host_cleaned'])
+    speciesNames = list(df['species'].value_counts().index)
+    with open(outputName, 'w') as f: 
+        for name in speciesNames: 
+            f.write(name + "\n")
+    
+    # split file into groups of 15,000
+    command = "split -a 1 -l 15000 " + outputName + " microSpecies"
+    os.system(command)
+
+def main():
+    splitSpecies(sys.argv[1], sys.argv[2])
+
+main()
